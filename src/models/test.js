3121bbaf62a64c8047c8086fe0a51ce6845f4dfc
Kris Hedstrom <kris@nomadhealth.com>
2017-11-27 17:29:42 -0500

    some example sequelize-cli files

---
+++ src/models/test.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Test = sequelize.define('Test', {
+    firstName: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Test;
+};
