dedf8d785270b790785032818f3147a1603bb6c3
Kristoffer Hedstrom <kris@nomadhealth.com>
2018-07-28 20:33:53 -0400

    Use graphql-sequelize connection for items

--- src/schema/user/type.js
+++ src/schema/user/type.js
@@ -4,23 +4,60 @@ import {
   connectionFromPromisedArray
 } from "graphql-relay";
 
-import { GraphQLObjectType, GraphQLString, GraphQLInt } from "graphql";
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+  GraphQLEnumType
+} from "graphql";
+import { createConnection } from "graphql-sequelize";
 
+import models from "../../models";
 import ItemType from "../item/type";
 import TagType from "../tag/type";
+import { auth } from "../../services/auth";
 
-const { connectionType: UserItemsConnection } = connectionDefinitions({
-  name: "UserItemsConnection",
-  nodeType: ItemType
-});
+const { User, Item } = models;
 
 const { connectionType: UserTagsConnection } = connectionDefinitions({
-  name: "UserTagsConnection",
+  name: "UserTags",
   nodeType: TagType
 });
 
+const userItemConnection = createConnection({
+  name: "UserItems",
+  nodeType: ItemType,
+  target: Item, // Can be an association for parent related connections or a model for "anonymous" connections
+  // if no orderBy is specified the model primary key will be used.
+  orderBy: new GraphQLEnumType({
+    name: "UserItemsOrderBy",
+    values: {
+      DATE: { value: ["date", "DESC"] }, // The first ENUM value will be the default order. The direction will be used for `first`, will automatically be inversed for `last` lookups.
+      AMOUNT: { value: ["amount", "DESC"] }
+    }
+  }),
+  before: (findOptions, args, context) => {
+    const id = auth(context);
+    findOptions.where = { UserId: id };
+    return findOptions;
+  },
+  connectionFields: {
+    total: {
+      type: GraphQLInt,
+      resolve: ({ source }) => {
+        /*
+         * We return a object containing the source, edges and more as the connection result
+         * You there for need to extract source from the usual source argument
+         */
+        return source.countItems();
+      }
+    }
+  }
+});
+
 const UserType = new GraphQLObjectType({
-  name: "User",
+  name: User.name,
   description: "This represents a User",
   fields: () => {
     return {
@@ -33,13 +70,11 @@ const UserType = new GraphQLObjectType({
       token: {
         type: GraphQLString
       },
-      items: {
-        type: UserItemsConnection,
+      item: {
         description: "The user's items",
-        args: connectionArgs,
-        resolve(user, args) {
-          return connectionFromPromisedArray(user.getItems(), args);
-        }
+        type: userItemConnection.connectionType,
+        args: userItemConnection.connectionArgs,
+        resolve: userItemConnection.resolve
       },
       tags: {
         type: UserTagsConnection,
