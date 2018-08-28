f4d88e8f07170ac7511df4b7505bfb1f81f4282d
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2018-08-27 22:30:17 -0400

    add more colors

--- src/utils/color.js
+++ src/utils/color.js
@@ -1,4 +1,14 @@
-export const COLORS = ["#F2CB6C", "#F27B6C", "#6CD5F2", "#4A90E2"];
+export const COLORS = [
+  "#F2CB6C",
+  "#F27B6C",
+  "#6CD5F2",
+  "#4A90E2",
+  "#f26ca1",
+  "#383971",
+  "#6f9cab",
+  "#3a0d2f",
+  "#b2cc7e"
+];
 
 export const randomValue = values => {
   return values[Math.floor(Math.random() * values.length)];
